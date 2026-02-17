0b06b496a75babcb39982e2e5530fd96294cd12b
Sameh92 <Samehsamara92@gmail.com>
2026-02-17 21:48:21 +0800

    test

--- CI-CD FirstExample/Models/ErrorViewModel.cs
+++ CI-CD FirstExample/Models/ErrorViewModel.cs
@@ -2,6 +2,10 @@ namespace CI_CD_FirstExample.Models
 {
     public class ErrorViewModel
     {
+
+        public ErrorViewModel()
+        {
+        }
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
